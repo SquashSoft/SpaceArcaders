27ce393dab6eb932668999c48f301e6302ae7111
awhipple <a.rgwhip@gmail.com>
2013-08-23 14:33:07 -0700

    Just initiated project

---
+++ SpaceArcaders/src/net/awhipple/spacearcaders/SpaceArcaders.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.awhipple.spacearcaders;
+
+/**
+ *
+ * @author Aaron
+ */
+public class SpaceArcaders {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Project Started!");
+    }
+}
